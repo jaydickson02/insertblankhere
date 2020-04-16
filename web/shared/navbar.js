875c71245e9317a3a2d72bf305a30f9c6add6de5
Jay Dickson <jaydickson02@gmail.com>
2020-04-16 10:54:23 +1000

    Allowed for active links in navbar

--- web/shared/navbar.js
+++ web/shared/navbar.js
@@ -4,19 +4,23 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
-let navbar = () => {
+let navbar = (props) => {
+
     return(
-        <Navbar bg="light" varient="light" expand="lg">
+        <Container>
+        <Navbar varient="light" expand="sm" style={{height: '80px'}}>
         <Navbar.Brand href="/">Presence</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav className="justify-content-end"  activeKey={props.activeLink}>
+            <Nav.Link href="/">Articles</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      </Container>
     )
 }
 
