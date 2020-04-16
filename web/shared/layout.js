948af46bcf657932e39f5cbb4fc0119053e56143
Jay Dickson <jaydickson02@gmail.com>
2020-04-16 10:54:09 +1000

    Added a footer to the webpages

--- web/shared/layout.js
+++ web/shared/layout.js
@@ -2,25 +2,30 @@
 import Navbar from './navbar';
 import Head from './head';
 import Container from 'react-bootstrap/Container'
+import Footer from './footer'
 
 let layout = (props) => {
+
     if(props.structure == false){
         return(
             <div>
                 <Head />
-                <Navbar />
+                <Navbar activeLink={props.activeLink}/>
                 {props.children}
+                <Footer />
             </div>
             )
     }
+
     return(
-    <div>
-        <Head />
-        <Navbar />
-        <Container>
-        {props.children}
-        </Container>
-    </div>
+        <div>
+            <Head />
+            <Navbar activeLink={props.activeLink}/>
+            <Container>
+            {props.children}
+            </Container>
+            <Footer />
+        </div>
     )
 }
 
