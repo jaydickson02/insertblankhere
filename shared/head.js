2f0d33cca41523af278413f32031640559467df5
Jay Dickson <jaydickson02@gmail.com>
2021-01-12 13:16:41 +1100

    Edited title

--- shared/head.js
+++ shared/head.js
@@ -10,6 +10,7 @@ let head = () => {
                 integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
                 crossorigin="anonymous"
             />
+            <title>insert blank here</title>
         </head>
     </div>
     )
